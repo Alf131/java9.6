24d850e27081c4d8aa810db7f09d27094d9f2c03
Alf Chlopecki <chlopecki13@gmail.com>
2017-06-14 20:14:31 +0200

    working

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 add.addEventListener('click', function() {
   var element = document.createElement('li');
-  element.innerHTML = 'item';
+  var itemLength = document.getElementsByTagName('li').length;
+  element.innerHTML = 'item ' + itemLength ;
   list.appendChild(element);
 })
